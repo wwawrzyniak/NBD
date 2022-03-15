a0c04eb2eb07ea3766d67d0639d51af3ed5ca04c
Weronika <s19515@pjwstk.edu.pl>
2022-03-15 21:53:40 +0100

    first commit

---
+++ 1/zapytanie_5.js
@@ -0,0 +1,1 @@
+printjson(db.people.find({  birth_date: {$gt: '2000-01-01T00:00:00Z', $lt: '2999-12-31T23:59:59Z'} }, { _id: 0, first_name: 1, last_name: 1, location:{ city : 1} }).toArray())
---
+++ 1/zapytanie_6.js
@@ -0,0 +1,21 @@
+printjson(db.people.insert(
+{"sex":"Female",
+"first_name":"Weronika",
+"last_name":"Wawrzyniak",
+"job":"Salesforce Developer",
+"email":"s19515@pjwstk.edu.pl",
+"location":
+{"city":"Warsaw",
+"address":
+{"streetname":"Piekna","streetnumber":"424"}},
+"description":"Lorem Ipsum",
+"height":"171.61",
+"weight":"55.3",
+"birth_date":"1998-12-05T05:44:11Z",
+"nationality":"Poland",
+"credit":[{"type":"laser","number":"5586087151891962","currency":"PLN","balance":"5512.36"},
+{"type":"bankcard","number":"5010120787942724","currency":"PLN","balance":"5190.65"},
+{"type":"mastercard","number":"201768421731919","currency":"PLN","balance":"6414.82"},
+{"type":"americanexpress","number":"3582797025806663","currency":"PLN","balance":"6365.42"},
+{"type":"jcb","number":"6376881458117617","currency":"PLN","balance":"5408.84"}]}
+))
