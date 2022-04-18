eb71eeca62fd3b2ab0896007b48fd15b8a6f2dd9
Weronika <s19515@pjwstk.edu.pl>
2022-04-18 20:24:46 +0200

    Delete zapytanie_2_A.js

--- zapytanie_2_A.js
+++
@@ -1,3 +0,0 @@
- printjson(db.people.aggregate(
-     [{"$unwind" : "$credit" },{"$group" : {"_id" : "$credit.currency","sumOfBalance" : {"$sum" :{ $toDouble:"$credit.balance"}}}}]
-     ).toArray())
