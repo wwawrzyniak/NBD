eb04baa19261c6f1de07d9cde89004d8ef3d78c7
Weronika <s19515@pjwstk.edu.pl>
2022-04-18 20:25:16 +0200

    Delete zapytanie_4_A.js

--- zapytanie_4_A.js
+++
@@ -1,5 +0,0 @@
-printjson(db.people.aggregate([{$group:{_id:'$nationality', 
-MinBMI:{$min: {$divide : [{ $toDouble:"$weight"}, { $pow : [{ $divide : [{ $toDouble:"$height"},100]},2]}]}}, 
-AvgBMI:{$avg: {$divide : [{ $toDouble:"$weight"}, { $pow : [{ $divide : [{ $toDouble:"$height"},100]},2]}]}},
-MaxBMI:{$max: {$divide : [{ $toDouble:"$weight"}, { $pow:  [{ $divide : [{ $toDouble:"$height"},100]},2]}]}}}}])
-.toArray())
