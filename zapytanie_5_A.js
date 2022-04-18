a0275451a4cbf95a3ca50dc0375f63a8e20080df
Weronika <s19515@pjwstk.edu.pl>
2022-04-18 20:25:31 +0200

    Delete zapytanie_5_A.js

--- zapytanie_5_A.js
+++
@@ -1,7 +0,0 @@
-printjson(db.people.aggregate(
-[{$unwind : "$credit"},
-{$match : {"sex": "Female", "nationality" : "Poland"}},
-{$group : {_id: "$credit.currency", 
-sum : {$sum : { $toDouble:"$credit.balance"}  }, 
-average : {$avg : { $toDouble: "$credit.balance"}} }}]
-).toArray())
